d2d22e24e81651e9651c7fe3dff8ec23d6d2aad5
belizwp <belizwp@live.com>
2017-11-15 16:14:56 +0700

    remove unmessage

--- app/src/test/java/cc/somkiat/basicunittesting/NameValidationPassTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/NameValidationPassTest.java
@@ -13,30 +13,30 @@ public class NameValidationPassTest {
     @Test
     public void singleLowerCaseNameShouldPass() throws BaseValidationException {
         String name = "b";
-        assertTrue("", nameValidator.validate(name));
+        assertTrue(nameValidator.validate(name));
     }
 
     @Test
     public void shortLowerCaseNameShouldPass() throws BaseValidationException {
         String name = "bell";
-        assertTrue("", nameValidator.validate(name));
+        assertTrue(nameValidator.validate(name));
     }
 
     @Test
     public void singleUpperCaseNameShouldPass() throws BaseValidationException {
         String name = "B";
-        assertTrue("", nameValidator.validate(name));
+        assertTrue(nameValidator.validate(name));
     }
 
     @Test
     public void shortUpperCaseNameShouldPass() throws BaseValidationException {
         String name = "BELL";
-        assertTrue("", nameValidator.validate(name));
+        assertTrue(nameValidator.validate(name));
     }
 
     @Test
     public void shortUpperAndLowerCaseNameShouldPass() throws BaseValidationException {
         String name = "Belizwp";
-        assertTrue("", nameValidator.validate(name));
+        assertTrue(nameValidator.validate(name));
     }
 }
